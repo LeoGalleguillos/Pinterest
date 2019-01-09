71f7a404be5d42da5cef4cfb898ded52fa9125a2
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-09 22:40:51 +0000

    Add name column to app table

--- sql/test/app/create.sql
+++ sql/test/app/create.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `app` (
-    `app_id` bigint unsigned,
-    `app_secret` varchar(255) default null,
+    `app_id` bigint unsigned not null,
+    `app_secret` varchar(255) not null,
+    `name` varchar(255) not null,
     PRIMARY KEY (`app_id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
