0f74bf11e2659cb50e074169898398d274024322
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-09 21:47:33 +0000

    Write sql for app_user table

---
+++ sql/test/app_user/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `app_user` (
+    `app_id` bigint unsigned not null,
+    `pinterest_user_id` bigint unsigned not null,
+    `access_token` varchar(255) not null,
+    PRIMARY KEY (`app_id`, `pinterest_user_id`)
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
