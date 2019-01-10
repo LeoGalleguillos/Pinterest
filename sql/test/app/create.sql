b823a4630fed7ac2e881d5e68f9a703b25ba32d1
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-10 14:53:09 +0000

    Add index on name column

--- sql/test/app/create.sql
+++ sql/test/app/create.sql
@@ -2,5 +2,6 @@ CREATE TABLE `app` (
     `app_id` bigint unsigned not null,
     `app_secret` varchar(255) not null,
     `name` varchar(255) not null,
-    PRIMARY KEY (`app_id`)
+    PRIMARY KEY (`app_id`),
+    INDEX `name` (`name`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
