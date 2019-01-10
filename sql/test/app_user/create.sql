d110daf6a9ad5524f6a0da5df42fda0074e7baba
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-10 14:56:22 +0000

    Add index on user_id column

--- sql/test/app_user/create.sql
+++ sql/test/app_user/create.sql
@@ -2,5 +2,6 @@ CREATE TABLE `app_user` (
     `app_id` bigint unsigned not null,
     `user_id` bigint unsigned not null,
     `access_token` varchar(255) not null,
-    PRIMARY KEY (`app_id`, `pinterest_user_id`)
+    PRIMARY KEY (`app_id`, `pinterest_user_id`),
+    INDEX `user_id` (`user_id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
