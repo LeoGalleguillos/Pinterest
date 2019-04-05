1a30a076fa989d1d587172861621fa04b0f90c5c
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-04-05 19:34:37 +0000

    Add `denied` column to app table

--- sql/test/app/create.sql
+++ sql/test/app/create.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `app` (
-    `app_id` bigint unsigned not null,
-    `app_secret` varchar(255) not null,
-    `name` varchar(255) not null,
+    `app_id` bigint unsigned NOT NULL,
+    `app_secret` varchar(255) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `denied` tinyint(1) NOT NULL DEFAULT '0',
     PRIMARY KEY (`app_id`),
     INDEX `name` (`name`)
-) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
